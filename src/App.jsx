ca5a41227cba182cbf1b1568e8e8f414d0c693e8
tanvir35web <islammdtanvirul@gmail.com>
2023-12-09 15:43:51 +0600

    image change using local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import reactImg from './assets/android2.svg'
 
 
 const operatingSystem = ['Windows', 'Mac', 'Linux', 'TV', 'Android', 'Watch'];
@@ -15,13 +16,13 @@ const randomSelect = operatingSystem[getRandomInt(5)];
 function App() {
 
   return (
-    <>
+    <div>
      
-    <img src="https://www.androidpolice.com/wp-content/uploads/2014/11/nexus2cee_android-studio-1.png" alt="demo image" />
+    <img src={reactImg} alt="demo image" />
     <h1> {randomSelect} Operating System</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa corporis impedit sequi consectetur repellat vel. Magni suscipit obcaecati facilis minus architecto repellat fugiat. Facilis porro ipsa aliquid voluptate ducimus provident minima. Illo, nostrum perferendis provident delectus nulla esse dolorum vero alias fugiat, officia excepturi eius, non consequuntur illum voluptate corporis?</p>
       
-    </>
+    </div>
   )
 }
 
