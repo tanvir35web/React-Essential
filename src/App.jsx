62c68707639083038b7b3531c4aa107c112e5e48
tanvir35web <islammdtanvirul@gmail.com>
2023-12-09 15:30:46 +0600

    os change dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@
 import './App.css'
 
 
+const operatingSystem = ['Windows', 'Mac', 'Linux', 'TV', 'Android', 'Watch'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+const randomSelect = operatingSystem[getRandomInt(5)];
+
+// const addSpace = operatingSystem.map((item) => item + ' ');
+
 function App() {
 
   return (
     <>
      
     <img src="https://www.androidpolice.com/wp-content/uploads/2014/11/nexus2cee_android-studio-1.png" alt="demo image" />
-    <h1>Android Operating System</h1>
+    <h1> {randomSelect} Operating System</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa corporis impedit sequi consectetur repellat vel. Magni suscipit obcaecati facilis minus architecto repellat fugiat. Facilis porro ipsa aliquid voluptate ducimus provident minima. Illo, nostrum perferendis provident delectus nulla esse dolorum vero alias fugiat, officia excepturi eius, non consequuntur illum voluptate corporis?</p>
       
     </>
